8ca40cb3ec32e2f95faec0d78fe9515b4daf5141
phongkham <nguyenthitinh.ou@gmail.com>
2016-12-27 17:53:21 +0700

    Them form NXB

---
+++ quanlynhasach/frmnxb.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace QLNHASACH
+{
+    public partial class frmnxb : Form
+    {
+        string cnStr;
+        SqlConnection cn;
+        DataSet ds;
+        // DataTable chungloai;
+        SqlCommand cmd = new SqlCommand();
+        public frmnxb()
+        {
+            InitializeComponent();
+        }
+
+        private void frmnxb_Load(object sender, EventArgs e)
+        {
+            cnStr = ConfigurationManager.ConnectionStrings["cnStr"].ConnectionString;
+            cn = new SqlConnection(cnStr);
+            string sql = "SELECT * FROM NXB";
+
+            dgvnxb.DataSource = hienthi(sql).Tables[0];
+            dgvnxb.Columns[0].HeaderText = "Mã NXB";
+            dgvnxb.Columns[0].Width = 75;
+            dgvnxb.Columns[1].HeaderText = "Tên Nhà Xuất Bản";
+            dgvnxb.Columns[1].Width = 200;
+            dgvnxb.Columns[2].HeaderText = "Địa Chỉ";
+            dgvnxb.Columns[3].HeaderText = "Giới Thiệu";
+
+
+
+        }
+        private DataSet hienthi(string sql)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "");
+                // throw;
+                return null;
+            }
+        }
+
+        private void dgvnxb_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int index = e.RowIndex;
+                txtmanxb.Text = dgvnxb.Rows[index].Cells[0].Value.ToString();
+                txttennxb.Text = dgvnxb.Rows[index].Cells[1].Value.ToString();
+                txtdc.Text = dgvnxb.Rows[index].Cells[2].Value.ToString();
+                txtmota.Text = dgvnxb.Rows[index].Cells[3].Value.ToString();
+              
+            }
+            catch
+            {
+                //  MessageBox.Show(ex + "");
+                //throw;
+            }
+        }
+
+        private void btdel_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            try
+            {
+                string sqldel = "delete from NXB where MANXB='" + txtmanxb.Text + "'";
+                SqlCommand cmd = new SqlCommand(sqldel, cn);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Xóa Nhà Xuất Bản Thành Công ! ");
+
+                string sql = "SELECT * FROM NXB";
+                dgvnxb.DataSource = hienthi(sql).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "");
+                // throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void bttsua_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            try
+            {
+                string sqlupda = "update NXB set TENNXB= N'"+txttennxb.Text+"', DIACHI= N'"+ txtdc.Text +"', GIOITHIEU = N'"+ txtmota.Text +"' where MANXB = N'"+ txtmanxb.Text +"'";
+                    
+                SqlCommand cmd = new SqlCommand(sqlupda, cn);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Sửa Nhà Xuất Bản Thành Công ! ");
+
+                string sql = "SELECT * FROM NXB";
+                dgvnxb.DataSource = hienthi(sql).Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "");
+                //throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void btadd_Click(object sender, EventArgs e)
+        {
+            frmnxb_them newFrm = new frmnxb_them();
+            newFrm.ShowDialog();
+
+            string sql = "SELECT * FROM NXB";
+            dgvnxb.DataSource = hienthi(sql).Tables[0];
+        }
+    }
+}
---
+++ quanlynhasach/frmnxb_them.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace QLNHASACH
+{
+    partial class frmnxb_them
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtdc = new System.Windows.Forms.TextBox();
+            this.txttennxb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmota = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtdc
+            // 
+            this.txtdc.Location = new System.Drawing.Point(130, 81);
+            this.txtdc.Multiline = true;
+            this.txtdc.Name = "txtdc";
+            this.txtdc.Size = new System.Drawing.Size(252, 53);
+            this.txtdc.TabIndex = 4;
+            // 
+            // txttennxb
+            // 
+            this.txttennxb.Location = new System.Drawing.Point(130, 33);
+            this.txttennxb.Name = "txttennxb";
+            this.txttennxb.Size = new System.Drawing.Size(118, 20);
+            this.txttennxb.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Địa Chỉ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên NXB:";
+            // 
+            // txtmota
+            // 
+            this.txtmota.Location = new System.Drawing.Point(130, 160);
+            this.txtmota.Multiline = true;
+            this.txtmota.Name = "txtmota";
+            this.txtmota.Size = new System.Drawing.Size(252, 61);
+            this.txtmota.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Giới Thiệu:";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Image = global::QLNHASACH.Properties.Resources.add2;
+            this.button1.Location = new System.Drawing.Point(130, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 54);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Thêm";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Image = global::QLNHASACH.Properties.Resources.exit;
+            this.button2.Location = new System.Drawing.Point(266, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 54);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Đóng";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmnxb_them
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 316);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtmota);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtdc);
+            this.Controls.Add(this.txttennxb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "frmnxb_them";
+            this.Text = "Thêm Nhà Xuất Bản";
+            this.Load += new System.EventHandler(this.frmnxb_them_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtdc;
+        private System.Windows.Forms.TextBox txttennxb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmota;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
